fd4e285997982cca9e82587d44d947f2ef50c914
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-03-11 01:00:20 -0600

    Added products view

--- src/main/java/com/cpro/retailplaygame/RetailplaygameApplication.java
+++ src/main/java/com/cpro/retailplaygame/RetailplaygameApplication.java
@@ -3,7 +3,7 @@ package com.cpro.retailplaygame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.cpro.retailplaygame")
 public class RetailplaygameApplication {
 
 	public static void main(String[] args) {
