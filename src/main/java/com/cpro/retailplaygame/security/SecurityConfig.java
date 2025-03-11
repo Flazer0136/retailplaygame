d69d268196539d4461b5e3a4673d986c550a2a33
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-03-10 21:56:48 -0600

    Implemented InMemory Auth

--- src/main/java/com/cpro/retailplaygame/security/SecurityConfig.java
+++ src/main/java/com/cpro/retailplaygame/security/SecurityConfig.java
@@ -5,46 +5,82 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// InMemory Auth
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 // JDBC Auth
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    // JDBC Auth
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    // InMemory Auth
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        JdbcUserDetailsManager userDetailsService = new JdbcUserDetailsManager(dataSource);
+    public InMemoryUserDetailsManager userDetailsManager() {
 
-        // Define your SQL query for loading users and authorities (roles)
-        userDetailsService.setUsersByUsernameQuery(
-                "SELECT username, password, enabled FROM users WHERE username = ?");
+        UserDetails customer = User.builder()
+                .username("customer")
+                .password("{noop}test123")
+                .roles("CUSTOMER")
+                .build();
 
-        userDetailsService.setAuthoritiesByUsernameQuery(
-                "SELECT u.username, a.authority FROM users u " +
-                        "JOIN authorities a ON u.userID = a.userID WHERE u.username = ?");
+        UserDetails owner = User.builder()
+                .username("owner")
+                .password("{noop}test123")
+                .roles("CUSTOMER", "OWNER")
+                .build();
 
-        return userDetailsService;
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}test123")
+                .roles("CUSTOMER", "OWNER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(customer, owner, admin);
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/products").hasRole("CUSTOMER")
-                .antMatchers(HttpMethod.GET, "/api/products/**").hasRole("CUSTOMER")
-                .antMatchers(HttpMethod.POST, "/api/products").hasRole("OWNER")
-                .antMatchers(HttpMethod.PUT, "/api/products").hasRole("OWNER")
-                .antMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();  // Use basic HTTP authentication
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/products").hasRole("CUSTOMER")
+                        .requestMatchers(HttpMethod.GET, "api/products/**").hasRole("CUSTOMER")
+                        .requestMatchers(HttpMethod.POST, "api/products").hasRole("OWNER")
+                        .requestMatchers(HttpMethod.PUT, "api/products").hasRole("OWNER")
+                        .requestMatchers(HttpMethod.DELETE, "api/products/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/api/products").hasRole("CUSTOMER")
+//                .antMatchers(HttpMethod.GET, "/api/products/**").hasRole("CUSTOMER")
+//                .antMatchers(HttpMethod.POST, "/api/products").hasRole("OWNER")
+//                .antMatchers(HttpMethod.PUT, "/api/products").hasRole("OWNER")
+//                .antMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();  // Use basic HTTP authentication
+//
+//        return http.build();
+//    }
 }
