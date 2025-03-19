95dcca22d60501a221de97094f4c2e4d717ac941
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-03-18 19:54:12 -0600

    Added coupon feature

--- src/main/java/com/cpro/retailplaygame/repository/CouponRepository.java
+++ src/main/java/com/cpro/retailplaygame/repository/CouponRepository.java
@@ -3,5 +3,8 @@ package com.cpro.retailplaygame.repository;
 import com.cpro.retailplaygame.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<Coupon> findByCouponCode(String couponCode);
 }
